e992ec75e6f04e1964d69427a72b7c9ec7703a3e
Glen Cooper <glen@glencooper.com>
2020-02-09 17:51:14 -0500

    Add ts.sh via upload
    This is how ts.sh appeared as of 20200209T225106Z

---
+++ ts.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# 20200103T193701Z ts.sh script, by Glen Cooper, glencooper.com
+# 
+# DESCRIPTION: Use this to get the timestamp now, or for a file using the timestamps.glencooper.com format
+#
+# HISTORY:
+# 20200131T234830Z if "ts" if typed by itself on the cli, the script should return the current timestamp
+
+# see https://archive.ph/ksHkJ
+if test -z "$1"
+then  # No command-line arguments
+  date '+%Y%m%dT%H%M%SZ'
+else # First command-line argument is $1
+  target=$1
+  identify -verbose $1 | grep -i digit
+fi
+
